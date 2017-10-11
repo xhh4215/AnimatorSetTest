6b5bcdae6a735224cdb9d7aec1cdd103ebea4504
xhh_git <18734184215@163.com>
2017-10-11 10:19:05 +0800

    使用代码实现为list view的item添加统一的动画

--- app/src/main/java/com/example/xiaoheihei/FristActivity.java
+++ app/src/main/java/com/example/xiaoheihei/FristActivity.java
@@ -3,6 +3,9 @@ package com.example.xiaoheihei;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
+import android.view.animation.Animation;
+import android.view.animation.AnimationUtils;
+import android.view.animation.LayoutAnimationController;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
@@ -31,6 +34,13 @@ public class FristActivity extends AppCompatActivity {
                 mAdapter.addAll(getData());
             }
         });
+//         使用代码为listview的item添加统一的动画
+//        Animation animation =  AnimationUtils.loadAnimation(this,R.anim.slide_in_left);
+//        LayoutAnimationController controller = new LayoutAnimationController(animation);
+//        controller.setOrder(LayoutAnimationController.ORDER_NORMAL);
+//        controller.setDelay(0.3f);
+//        mListView.setLayoutAnimation(controller);
+//        mListView.startLayoutAnimation();
     }
 
     private List<String> getData() {
